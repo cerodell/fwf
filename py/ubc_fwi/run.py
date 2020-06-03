aab6cb886e6b83fa26b60da2490308f5219c113b
Chris Rodell <crodell@eoas.ubc.ca>
2020-06-03 10:48:51 -0700

    repo moved to BSC projects folder

--- py/ubc_fwi/run.py
+++ py/ubc_fwi/run.py
@@ -15,8 +15,6 @@ from context import data_dir, xr_dir, wrf_dir, tzone_dir, root_dir
 
 
 
-
-
 # %%
 """######### Open wrf_out.nc and write to zarr #############"""
 # wrf_file_dir = str(data_dir) + folder
@@ -27,19 +25,22 @@ coeff = FWF(wrf_file_dir, None, None)
 daily_file_dir  = coeff.daily()
 hourly_file_dir  = coeff.hourly()
 
-"""######### This Solves for daily_ds #############"""
+
+
+
+"""######### Open daily_ds #############"""
 daily_ds = xr.open_zarr(daily_file_dir)
 print(daily_ds)
 # print(np.nanmin(np.array(daily_ds.D)), "D final min")
 # print(np.nanmax(np.array(daily_ds.D)), "D final max")
 
 
-"""######### This Solves for hourly_ds #############"""
+"""######### Open hourly_ds #############"""
 hourly_ds = xr.open_zarr(hourly_file_dir)
 # print(hourly_ds)
 # print(hourly_ds.H)
 # print((hourly_ds.R))
-print((hourly_ds.S))
+print(np.nanmax(np.array(hourly_ds.S)))
 
 
 
@@ -48,25 +49,24 @@ print((hourly_ds.S))
 
 
 
-# wrf_file_dir = '/Volumes/CER/WFRT/FWI/Data/20190820'
-# coeff = FWF(wrf_file_dir, hourly_file_dir, daily_file_dir )
+wrf_file_dir = '/Volumes/CER/WFRT/FWI/Data/20190820'
+coeff = FWF(wrf_file_dir, hourly_file_dir, daily_file_dir )
+
+daily_file_dir  = coeff.daily()
+hourly_file_dir  = coeff.hourly()
+
+"""######### Open daily_ds #############"""
+daily_ds = xr.open_zarr(daily_file_dir)
+print(daily_ds)
+print(np.nanmin(np.array(daily_ds.D)), "D final min")
+print(np.nanmax(np.array(daily_ds.D)), "D final max")
+print((daily_ds.H.projection))
 
 
-
-# """######### This Solves for daily_ds #############"""
-# daily_file_dir  = coeff.daily()
-# daily_ds = xr.open_zarr(daily_file_dir)
-# print(daily_ds)
-# print(np.nanmin(np.array(daily_ds.D)), "D final min")
-# print(np.nanmax(np.array(daily_ds.D)), "D final max")
-# print((daily_ds.H.projection))
-
-
-# """######### This Solves for hourly_ds #############"""
-# hourly_file_dir  = coeff.hourly()
-# hourly_ds = xr.open_zarr(hourly_file_dir)
-# print(hourly_ds)
-# print(hourly_ds.F)
+"""######### Open hourly_ds #############"""
+hourly_ds = xr.open_zarr(hourly_file_dir)
+print(hourly_ds)
+print(np.nanmax(np.array(hourly_ds.S)))
 
 
 
