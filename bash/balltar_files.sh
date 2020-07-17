ccc10fc5ccf4337f840fb8768e212a2aca6b6c38
Christopher Rodell <crodell@eoas.ubc.ca>
2020-07-17 13:25:03 -0200

    issue with run_fwf_all.sh, looking to replace with a run_fwf_all.py

--- bash/balltar_files.sh
+++ bash/balltar_files.sh
@@ -6,7 +6,7 @@ cd /bluesky/fireweather/fwf/data/xr
 echo "making fwf-daily tgz at: --> $(date '+%Y-%m-%dT%H')"
 tar -czvf fwf-daily-$(date '+%Y%m%d00').tgz fwf-daily-$(date '+%Y%m%d00').zarr
 mv fwf-daily-$(date '+%Y%m%d00').tgz /bluesky/archive/fireweather/data/
-rm -r fwf-daily-$(date -d "yesterday" '+%Y%m%d00').zarr
+rm -r fwf-daily-$(date  -d "2 days ago" '+%Y%m%d00').zarr
 echo "fwf-daily tgz made and mv at: --> $(date '+%Y-%m-%dT%H')"
 
 echo "making fwf-hourly tgz at: --> $(date '+%Y%m%dT%H')"
