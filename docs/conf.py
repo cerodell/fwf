da6e9df389647b5dce06f35051cf2b79d4b75686
Chris Rodell <crodell@eoas.ubc.ca>
2020-07-29 09:45:14 -0700

    work on docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Fire Weather Forecast Model'
-copyright = '2020, Chris Rodell'
-author = 'Chris Rodell'
+project = 'GitLab Pages with Sphinx'
+copyright = '2020, Miles Davis'
+author = 'Miles Davis'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = 'https://gitlab.com/pages/sphinx'
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,8 +53,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-
-## Add Markdown
-extensions = ['recommonmark']
-
