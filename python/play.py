2afce52d820f2d78420940ad148d98f60702edfe
Christopher Rodell <crodell@eoas.ubc.ca>
2021-02-12 14:35:33 -0200

    tweaks to play script, push before lookng at dev branch

--- python/play.py
+++ python/play.py
@@ -2,6 +2,6 @@ import numpy as np
 import xarray as xr
 
 
+filein = "/bluesky/fireweather/fwf/data/FWF-WAN00CG-01/fwf-hourly-d03-2021021106.zarr"
 
-
-ds = xr.open_dataset('/bluesky/working/wrf2arl/WAN00CG-01/2021020800/download/wrfout_d03_2021-02-10_07:00:00')
+ds = xr.open_zarr(filein)
