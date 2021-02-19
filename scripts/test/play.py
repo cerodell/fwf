f755a7ff53186bdc2d63867b00a9bc19935aa266
Christopher Rodell <crodell@eoas.ubc.ca>
2021-02-19 15:41:37 -0200

    :rocket: ploting script for bluesky domains

--- scripts/test/play.py
+++ scripts/test/play.py
@@ -10,9 +10,12 @@ from datetime import datetime, date, timedelta
 
 
 domain = "d02"
-file_date = "20210214"
-# file_dir = str(fwf_zarr_dir) + f"/fwf-daily-{domain}-{file_date}06.zarr"
-# ds = xr.open_zarr(file_dir)
+date = pd.Timestamp("today")
+# date = pd.Timestamp(2021, 2, 15)
+forecast_date = date.strftime("%Y%m%d06")
+
+file_dir = str(fwf_zarr_dir) + f"/fwf-hourly-{domain}-{forecast_date}.zarr"
+ds = xr.open_zarr(file_dir)
 
 # ds = ds.isel(time=0)
 
@@ -23,23 +26,23 @@ file_date = "20210214"
 # wrf_ds = xr.open_zarr(file_dir)
 
 
-ds = xr.open_zarr(
-    str(data_dir) + "/intercomp/" + f"intercomp-{domain}-{file_date}.zarr"
-)
+# ds = xr.open_zarr(
+#     str(data_dir) + "/intercomp/" + f"intercomp-{domain}-{file_date}.zarr"
+# )
 
-for var in list(ds):
-    # print(f"max of {var}: {str(ds.Time.values)}")
-    print(f"max of {var}: {float(ds[var].max(skipna= True))}")
-    print(f"min of {var}: {float(ds[var].min(skipna= True))}")
-    print(f"mean of {var}: {float(ds[var].mean(skipna= True))}")
+# for var in list(ds):
+#     # print(f"max of {var}: {str(ds.Time.values)}")
+#     print(f"max of {var}: {float(ds[var].max(skipna= True))}")
+#     print(f"min of {var}: {float(ds[var].min(skipna= True))}")
+#     print(f"mean of {var}: {float(ds[var].mean(skipna= True))}")
 
-var = "DC_day1"
-for i in range(len(ds.time)):
-    print(str(ds.time.values[i]))
-    print(float(ds.isel(time=i)[var].max(skipna=True)))
-    print(float(ds.isel(time=i)[var].min(skipna=True)))
-    print(float(ds.isel(time=i)[var].mean(skipna=True)))
+# var = "DC_day1"
+# for i in range(len(ds.time)):
+#     print(str(ds.time.values[i]))
+#     print(float(ds.isel(time=i)[var].max(skipna=True)))
+#     print(float(ds.isel(time=i)[var].min(skipna=True)))
+#     print(float(ds.isel(time=i)[var].mean(skipna=True)))
 
-air1d = ds[var].isel(wmo=200)
+# air1d = ds[var].isel(wmo=200)
 
-air1d.plot()
+# air1d.plot()
